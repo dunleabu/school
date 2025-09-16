4fc90fd099ee42420a7bd9a2de3c9d7c24a7e3d8
Andrew Dunleavy <andrewdunleavy@gmail.com>
2025-09-17 10:23:30 +1200

    renamed some types in Question.elm and implemented question class attributes for long division in LogDiv

--- src/LongDiv.elm
+++ src/LongDiv.elm
@@ -6,257 +6,46 @@ import Html exposing (Html, button, div, input, text, hr)
 import Html.Attributes exposing (name, type_, value, class)
 import Html.Events exposing (onClick, onInput)
 import Random
-
-
-type alias Model =
-    { rows : List Row, seed : Int }
-
-
-type QMsg
-    = Quotient Int String
-    | Remainder Int String
-
+import Question
 
 type Msg
-    = NewSeed String
-    | UpdateQuestion QMsg
-    | Cheat
+    = Quotient String
+    | Remainder String
 
 
-type Status
-    = Todo
-    | Incorrect
-    | Correct
 
 
-type Question
-    = Division { divisor : Int, dividend : Int, quotient : Maybe Int, remainder : Maybe Int }
+type alias Division
+    = { divisor : Int, dividend : Int, quotient : Maybe Int, remainder : Maybe Int }
 
-
-type alias Row =
-    { question : Question, status : Status, index : Int }
-
-
-checkQuestion : Question -> Status
-checkQuestion (Division d) =
-    case ( d.quotient, d.remainder ) of
-        ( Just q, Just r ) ->
-            let
-                a =
-                    r + q * d.divisor
-
-                _ =
-                    Debug.log "r = " r
-
-                _ =
-                    Debug.log "q = " q
-
-                _ =
-                    Debug.log "a = " a
-            in
-            if r + q * d.divisor == d.dividend then
-                Correct
-
-            else
-                Incorrect
-
-        _ ->
-            Todo
-
-
-solve : Question -> Question
-solve (Division d) =
-    Division { d | quotient = Just <| d.dividend // d.divisor, remainder = Just <| modBy d.divisor d.dividend }
-
-
-showAnswers row =
-    let
-        q = solve row.question
-    in
-    { row | question = q, status = checkQuestion q }
-
-
-updateQuotient : Maybe Int -> (Question -> Question)
-updateQuotient value =
-    \(Division d) -> Division { d | quotient = value }
-
-
-updateRemainder : Maybe Int -> (Question -> Question)
-updateRemainder value =
-    \(Division d) -> Division { d | remainder = value }
-
-
-makeRows : List Question -> List Row
-makeRows qs =
-    List.indexedMap
-        (\i q -> Row q Todo i)
-        qs
-
-
-unicode n =
-    " " ++ String.fromChar (Char.fromCode n) ++ " "
-
-
-divideSymbol =
-    unicode 0xF7
-
-
-tick =
-    unicode 0x2705
-
-
-cross =
-    unicode 0x274C
-
-
-qmark =
-    unicode 0x2753
-
-
-rowHtml : Row -> Html Msg
-rowHtml x =
-    questionHtml x.question x.index ++ statusHtml x.status |> div [class "question"]
-
-
-statusHtml : Status -> List (Html Msg)
-statusHtml s =
-    [ text <|
-        case s of
-            Todo ->
-                qmark
-
-            Incorrect ->
-                cross
-
-            Correct ->
-                tick
-    ]
-
-
-enterSeed : Html Msg
-enterSeed =
-    div [] [ text "seed = ", input [ type_ "number", name "seed", onInput NewSeed ] [] ]
-
-
-fv v =
-    case v of
-        Nothing ->
-            ""
-
-        Just i ->
-            String.fromInt i
-
-
-questionHtml : Question -> Int -> List (Html Msg)
-questionHtml (Division q) row =
-    [ divideText q |> text
-    , div [class "answer"]
-        [ input [ value (fv q.quotient), type_ "number", name "quotient", onInput (\x -> UpdateQuestion (Quotient row x)) ] []
-        , text " remainder "
-        , input [ value (fv q.remainder), type_ "number", name "remainder", onInput (\x -> UpdateQuestion (Remainder row x)) ] []
-        ]
-    ]
-
-
-divideText d =
-    String.fromInt d.dividend ++ divideSymbol ++ String.fromInt d.divisor ++ " = "
-
-
-randomQuestion : Random.Generator Question
-randomQuestion =
+generator : Random.Generator Division
+generator =
     Random.map3
         (\x y z ->
-            Division { divisor = x, dividend = x * y + z, quotient = Nothing, remainder = Nothing }
+            { divisor = x, dividend = x * y + z, quotient = Nothing, remainder = Nothing }
         )
         (Random.int 3 17)
         (Random.int 7 250)
         (Random.int 0 17)
 
+solve : Division -> Division
+solve d =
+    { d | quotient = Just <| d.dividend // d.divisor, remainder = Just <| modBy d.divisor d.dividend }
 
-randomQuestions : Random.Generator (List Question)
-randomQuestions =
-    Random.list 10 randomQuestion
+view: Division -> Question.Convert Msg -> Question.Qhtml Msg
+view d f =
+    {question = text "question"
+     ,answer = text "answer"}
 
+update : Division -> Msg -> Division
+update d m =
+    case m of
+        Quotient val -> {d | quotient = String.toInt val}
+        Remainder val -> {d | remainder = String.toInt val}
 
-cheat =
-    button [ onClick Cheat ] [ text "CHEAT!" ]
-
-
-view : Model -> Html Msg
-view model =
-    enterSeed
-        :: List.map rowHtml model.rows
-        ++ [cheat ]
-        |> List.intersperse (hr [] [])
-        |> div []
-
-
-updateRow : Int -> (Question -> Question) -> (Row -> Row)
-updateRow i f =
-    \r ->
-        if r.index == i then
-            let
-                q =
-                    f r.question
-            in
-            { r | question = q, status = checkQuestion q }
-
-        else
-            r
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NewSeed s ->
-            ( modelFromSeed <| String.toInt s, Cmd.none )
-
-        Cheat ->
-            ( { model | rows = List.map showAnswers model.rows }, Cmd.none )
-
-        UpdateQuestion qm ->
-            let
-                _ =
-                    Debug.log "message is" msg
-
-                f : Row -> Row
-                f =
-                    case qm of
-                        Quotient row data ->
-                            updateRow row (updateQuotient (String.toInt data))
-
-                        Remainder row data ->
-                            updateRow row (updateRemainder (String.toInt data))
-
-                _ =
-                    Debug.log "model = " (List.map f model.rows)
-            in
-            ( { model | rows = List.map f model.rows }, Cmd.none )
-
-
-initModel =
-    makeRows []
-
-
-modelFromSeed : Maybe Int -> Model
-modelFromSeed s =
-    let
-        seed =
-            Maybe.withDefault 0 s
-    in
-    { rows = makeRows <| randomValuesFromSeed seed, seed = seed }
-
-
-randomValuesFromSeed : Int -> List Question
-randomValuesFromSeed i =
-    Random.step randomQuestions (Random.initialSeed i) |> Tuple.first
-
-
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = \flags -> ( modelFromSeed <| Just 0, Cmd.none )
+class = Question.Class
+        { generator = generator
+        , solve = solve
         , view = view
         , update = update
-        , subscriptions = \model -> Sub.none
         }
--- src/Question.elm
+++ src/Question.elm
@@ -9,24 +9,26 @@ type Status
     | Incorrect
     | Correct
 
-type alias QuestionHtml m =
-    { question: Html m
-    , answer: Html m
+type alias Qhtml m =
+    { question: Html (Msg m)
+    , answer: Html (Msg m)
     }
 
 type alias Row q =
     {question : q, status: Status, id: Int}
 
-type RowMsg m
+type Msg m
     = QMsg Int m
     | NewSeed String
     | Cheat
+
+type alias Convert m = (m -> Msg m)
 
 type Class comparable_question msg
     = Class
         { generator : Random.Generator comparable_question
         , solve : comparable_question -> comparable_question
-        , view : comparable_question -> (msg -> RowMsg msg) -> QuestionHtml (RowMsg msg)
+        , view : comparable_question -> Convert msg -> Qhtml msg
         , update : comparable_question -> msg -> comparable_question
         }
 
@@ -36,7 +38,7 @@ check : Class comparable_q m -> comparable_q -> Status
 check (Class c) q =
     if q == c.solve q then Correct else Incorrect
 
-updateRow : Class comparable_q m -> RowMsg m -> Row comparable_q -> Row comparable_q
+updateRow : Class comparable_q m -> Msg m -> Row comparable_q -> Row comparable_q
 updateRow (Class c as x) rm r =
     case rm of
         (QMsg i m) ->
@@ -49,10 +51,10 @@ updateRow (Class c as x) rm r =
                 r
         _ -> r
 
-viewRow : Class comparable_q m -> Row comparable_q -> Html (RowMsg m)
+viewRow : Class comparable_q m -> Row comparable_q -> Html (Msg m)
 viewRow (Class c) r =
     let
-        h : QuestionHtml (RowMsg m)
+        h : Qhtml m
         h = c.view r.question (QMsg r.id)
     in
     div [] [h.question, h.answer]
