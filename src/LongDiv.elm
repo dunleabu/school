7f3220ddfe5237a86c51a743d3bd00be221cbff3
Andrew Dunleavy <andrewdunleavy@gmail.com>
2025-09-17 14:03:39 +1200

    rough, hard-coded example of creating Browser.element from Question.Class - dummy content

--- src/LongDiv.elm
+++ src/LongDiv.elm
@@ -2,21 +2,21 @@ module LongDiv exposing (main)
 
 import Browser
 import Debug
-import Html exposing (Html, button, div, input, text, hr)
-import Html.Attributes exposing (name, type_, value, class)
+import Html exposing (Html, button, div, hr, input, text)
+import Html.Attributes exposing (class, name, type_, value)
 import Html.Events exposing (onClick, onInput)
-import Random
 import Question
+import Random
+
 
 type Msg
     = Quotient String
     | Remainder String
 
 
+type alias Division =
+    { divisor : Int, dividend : Int, quotient : Maybe Int, remainder : Maybe Int }
 
-
-type alias Division
-    = { divisor : Int, dividend : Int, quotient : Maybe Int, remainder : Maybe Int }
 
 generator : Random.Generator Division
 generator =
@@ -28,24 +28,64 @@ generator =
         (Random.int 7 250)
         (Random.int 0 17)
 
+
 solve : Division -> Division
 solve d =
     { d | quotient = Just <| d.dividend // d.divisor, remainder = Just <| modBy d.divisor d.dividend }
 
-view: Division -> Question.Convert Msg -> Question.Qhtml Msg
+
+view : Division -> Question.Convert Msg -> Question.Qhtml Msg
 view d f =
-    {question = text "question"
-     ,answer = text "answer"}
+    { question = text "question"
+    , answer = text "answer"
+    }
+
 
 update : Division -> Msg -> Division
 update d m =
     case m of
-        Quotient val -> {d | quotient = String.toInt val}
-        Remainder val -> {d | remainder = String.toInt val}
+        Quotient val ->
+            { d | quotient = String.toInt val }
 
-class = Question.Class
+        Remainder val ->
+            { d | remainder = String.toInt val }
+
+
+class =
+    Question.Class
         { generator = generator
         , solve = solve
         , view = view
         , update = update
         }
+
+
+main : Program () (Question.Model Division) (Question.Msg Msg)
+main =
+    Browser.element
+        { init = \_ -> ( modelFromSeed generator (Just 0), Cmd.none )
+        , view = b_view class
+        , update = Question.updateModel class
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+modelFromSeed : Random.Generator q -> Maybe Int -> Question.Model q
+modelFromSeed g s =
+    let
+        seed =
+            Maybe.withDefault 0 s
+
+        gen =
+            Random.list 10 g
+
+        qs =
+            Random.step gen (Random.initialSeed seed) |> Tuple.first
+    in
+    { seed = seed, rows = List.indexedMap (\i q -> { question = q, status = Question.Todo, id = i }) qs }
+
+
+b_view : Question.Class q m -> Question.Model q -> Html (Question.Msg m)
+b_view c m =
+    div []
+        (List.map (Question.viewRow c) m.rows)
--- src/Question.elm
+++ src/Question.elm
@@ -9,52 +9,78 @@ type Status
     | Incorrect
     | Correct
 
+
 type alias Qhtml m =
-    { question: Html (Msg m)
-    , answer: Html (Msg m)
+    { question : Html (Msg m)
+    , answer : Html (Msg m)
     }
 
+
 type alias Row q =
-    {question : q, status: Status, id: Int}
+    { question : q, status : Status, id : Int }
+
 
 type Msg m
     = QMsg Int m
     | NewSeed String
     | Cheat
 
-type alias Convert m = (m -> Msg m)
 
-type Class comparable_question msg
+type alias Convert m =
+    m -> Msg m
+
+
+type Class zzz_question msg
     = Class
-        { generator : Random.Generator comparable_question
-        , solve : comparable_question -> comparable_question
-        , view : comparable_question -> Convert msg -> Qhtml msg
-        , update : comparable_question -> msg -> comparable_question
+        { generator : Random.Generator zzz_question
+        , solve : zzz_question -> zzz_question
+        , view : zzz_question -> Convert msg -> Qhtml msg
+        , update : zzz_question -> msg -> zzz_question
         }
 
 
+type alias Model zzz_question =
+    { seed : Int
+    , rows : List (Row zzz_question)
+    }
 
-check : Class comparable_q m -> comparable_q -> Status
+{-
+check : Class zzz_q m -> zzz_q -> Status
 check (Class c) q =
-    if q == c.solve q then Correct else Incorrect
+    if q == c.solve q then
+        Correct
 
-updateRow : Class comparable_q m -> Msg m -> Row comparable_q -> Row comparable_q
-updateRow (Class c as x) rm r =
-    case rm of
-        (QMsg i m) ->
-            if i == r.id then
+    else
+        Incorrect
+-}
+
+
+updateRow : Class zzz_q m -> Int -> m -> Row zzz_q -> Row zzz_q
+updateRow (Class c) id msg row=
+            if id == row.id then
                 let
-                    q = c.update r.question m
+                    q =
+                        c.update row.question msg
                 in
-                {r | question = q, status = check x q}
-            else
-                r
-        _ -> r
+                { row | question = q, status = if q == c.solve q then Correct else Incorrect }
 
-viewRow : Class comparable_q m -> Row comparable_q -> Html (Msg m)
+            else
+                row
+
+
+updateModel : Class zzz_q m -> Msg m -> Model zzz_q -> (Model zzz_q, Cmd (Msg m))
+updateModel c rm model =
+    case rm of
+        QMsg i m ->
+            ({model | rows = List.map (updateRow c i m) model.rows}, Cmd.none)
+        _ ->
+            (model, Cmd.none)
+
+viewRow : Class zzz_q m -> Row zzz_q -> Html (Msg m)
 viewRow (Class c) r =
     let
         h : Qhtml m
-        h = c.view r.question (QMsg r.id)
+        h =
+            c.view r.question (QMsg r.id)
     in
-    div [] [h.question, h.answer]
+    div [] [ h.question, h.answer ]
